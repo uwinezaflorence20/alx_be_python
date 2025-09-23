533035ea9e01e115341b477b768271343abf4c97
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2025-09-23 19:56:57 +0200

    done

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,22 +1,13 @@
 from datetime import datetime, timedelta
-
 def display_current_datetime():
-    """
-    Display the current date and time in 'YYYY-MM-DD HH:MM:SS' format.
-    """
+   
     current_date = datetime.now()
     formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
     print("Current date and time:", formatted_date)
     return current_date
 
 def calculate_future_date(current_date, days_to_add):
-    """
-    Calculate and display the future date after adding the specified number of days.
-    
-    Parameters:
-        current_date (datetime): The starting date
-        days_to_add (int): Number of days to add
-    """
+
     future_date = current_date + timedelta(days=days_to_add)
     print("Future date:", future_date.strftime("%Y-%m-%d"))
     return future_date
