e27181f0d3bbb72d6a3978eee54263b0581a2747
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2025-09-30 15:31:51 +0200

    done

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,22 @@
+class BankAccount:
+    def __init__(self, initial_balance=0.0):
+        self.__account_balance = initial_balance  # Encapsulated attribute
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+        else:
+            print("Deposit amount must be positive.")
+
+    def withdraw(self, amount):
+        if amount > self.__account_balance:
+            return False
+        elif amount <= 0:
+            print("Withdrawal amount must be positive.")
+            return False
+        else:
+            self.__account_balance -= amount
+            return True
+
+    def display_balance(self):
+        print(f"Current Balance: ${self.__account_balance:.2f}")
